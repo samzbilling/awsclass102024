0f934df3ed33146fbdb9a488f82cd46ba61b44fd
Tsega <60670021+tberhie@users.noreply.github.com>
2024-11-14 21:02:24 -0500

    Update main.tf

--- aws_class_midterm/modules/vpc/main.tf
+++ aws_class_midterm/modules/vpc/main.tf
@@ -1,27 +1,60 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
+provider "aws" {
+  region = var.region
+}
 
+# VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "aws-class-terraform-vpc"
+    Name = "my_vpc"
   }
 }
 
+# Public Subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet"
+  }
 }
 
+# Private Subnet
 resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private_subnet"
+  }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+# Internet Gateway
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my_ig"
+  }
 }
 
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+  tags = {
+    Name = "public_rt"
+  }
+}
 
-
+# Route Table Association for Public Subnet
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
